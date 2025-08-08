db0483523fae507df4ba4fc677520a3bb6b51c96
Petr Šimbakov <petrshimbakov@gmail.com>
2025-08-08 15:06:05 +0200

    Fix order log error messages

--- src/services/orders/order-log-service.ts
+++ src/services/orders/order-log-service.ts
@@ -6,9 +6,9 @@ export async function sendOrderLog(message: MessageCreateOptions) {
 	try {
 		const guild = client.guilds.cache.get(config.guildId) || (await client.guilds.fetch(config.guildId));
 		const channel = guild.channels.cache.get(config.channels.orderLog) || (await guild.channels.fetch(config.channels.orderLog));
-		if (!channel || !channel.isSendable()) throw new Error("modLog channel is not sendable!");
+		if (!channel || !channel.isSendable()) throw new Error("order-log-service: channel is not sendable!");
 		await channel.send(message);
 	} catch (err) {
-		console.error("[mod-log-service] Error while sending a order log:", err);
+		console.error("[order-log-service] Error while sending a order log:", err);
 	}
 }
